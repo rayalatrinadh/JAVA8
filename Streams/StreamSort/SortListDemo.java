77619b4344c4f7e1570f1e409d1501df2c1aab0b
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-01 22:15:56 -0500

    Collections sort using stream

---
+++ Streams/StreamSort/SortListDemo.java
@@ -0,0 +1,35 @@
+package com.trinadh.StreamSort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortListDemo {
+    public static void main(String args[]){
+
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(12);
+        list.add(3);
+        list.add(4);
+
+        //traditional approach to sort the list in ascending and descending order
+        Collections.sort(list);
+        System.out.println(list);
+        //output: [2, 3, 4, 12]
+
+        System.out.println("list in reverse order in traditional order: ");
+        Collections.reverse(list);
+        System.out.println(list);
+        //output: [12, 4, 3, 2]
+
+        System.out.println("sorting list using stream lambda functions: ");
+        list.stream().sorted().forEach(t->System.out.print(t + " "));
+        //output: 2 3 4 12
+
+        System.out.println("\nreverse order with stream function");
+        list.stream().sorted(Collections.reverseOrder()).forEach(t->System.out.print(t + " "));
+        //output: 12 4 3 2
+
+    }
+}
