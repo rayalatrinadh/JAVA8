05a27a8e5213e18879d058a8a9ab3964006c278f
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-03 20:15:26 -0500

    Sort Map Traditional Approach

---
+++ Streams/StreamSort/MapSort/SortMapDemoTraditional.java
@@ -0,0 +1,25 @@
+package com.trinadh.StreamSort.MapSort;
+
+import java.util.*;
+
+public class SortMapDemoTraditional {
+    public static void main(String args[]){
+        Map<String, Integer> map = new HashMap<>();
+        map.put("trinadh",7);
+        map.put("rayala",5);
+        map.put("rakesh",6);
+        map.put("kiran",7);
+
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+        Collections.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+        });
+
+        for(Map.Entry<String,Integer> entry: entries){
+            System.out.println(entry.getKey() +  " "+ entry.getValue());
+        }
+    }
+}
