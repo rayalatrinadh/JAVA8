a33a1adf6b4e6d392fa965b92b139aed8bd604af
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-05 06:58:30 -0500

    CustomData map Sort Traditional Approach

---
+++ Streams/StreamSort/MapSort/CustomSortMap.java
@@ -0,0 +1,26 @@
+package com.trinadh.StreamSort.MapSort;
+
+import com.trinadh.ApiExample.Employee;
+
+import java.util.*;
+
+public class CustomSortMap {
+    public static void main(String args[]){
+        Map<Employee, Integer> employeeMap = new TreeMap<>(new Comparator<Employee>(){
+
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return (int) (o2.getSalary()-o1.getSalary());
+            }
+        });
+
+        employeeMap.put(new Employee(123,"Trinadh","Swe",80000), 80);
+        employeeMap.put(new Employee(124,"Rayala","Swe",90000), 90);
+        employeeMap.put(new Employee(125,"Rakesh","Swe",100000), 100);
+        employeeMap.put(new Employee(126,"Kiran","Swe",110000), 110);
+
+         System.out.println(employeeMap);
+         //output: {Employee{id=126, name='Kiran', dept='Swe', salary=110000}=110, Employee{id=125, name='Rakesh', dept='Swe', salary=100000}=100, Employee{id=124, name='Rayala', dept='Swe', salary=90000}=90, Employee{id=123, name='Trinadh', dept='Swe', salary=80000}=80}
+
+    }
+}
