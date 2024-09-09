fab34a7d05b46dc91cdad882cf616b9d1a0efb2f
Trinadh Rayala <3nadhmail@gmail.com>
2024-09-08 23:04:54 -0500

    Optional Java8 Usage

--- Streams/OptionalDemo.java
+++ Streams/OptionalDemo.java
@@ -8,13 +8,31 @@ import java.util.Optional;
 public class OptionalDemo {
     public static void main(String[] args) {
 
-        Customer customer = new Customer(123,"trinadh", null , Arrays.asList("1233","111"));
+        Customer customer = new Customer(123, "trinadh", null, Arrays.asList("1233", "111"));
 
         Optional<Object> emptyOptional = Optional.empty();
         System.out.println(emptyOptional);
 
 
-        Optional<String> emailOptionalCheck = Optional.of(customer.getEmail());
-        System.out.println(emailOptionalCheck);
+//        Optional<String> emailOptionalCheck = Optional.of(customer.getEmail());
+//        System.out.println(emailOptionalCheck);
+
+        System.out.println("----------------");
+        Optional<String> emailOption2 = Optional.ofNullable(customer.getEmail());
+        System.out.println(emailOption2);
+
+
+
+
+        System.out.println(emailOption2.orElse("default@gmail.com"));
+
+        System.out.println(emailOption2.orElseGet(()->"default@gmail.com"));
+
+        System.out.println(emailOption2.map(String::toUpperCase).orElseGet(()->"default@gmail.com"));
+        System.out.println(emailOption2.orElseThrow(() -> new IllegalArgumentException("email not present")));
+
+
+
+
     }
 }
